78d0814b9caa81cd9303f4470a62ba77231ada67
Martin Boonk <martin.boonk@hjp-consulting.com>
2015-07-13 15:32:31 +0200

    added javadoc

--- com.hjp.osgi.remotecontrol/src/com/hjp/globaltester/control/AbstractRemoteControlHandler.java
+++ com.hjp.osgi.remotecontrol/src/com/hjp/globaltester/control/AbstractRemoteControlHandler.java
@@ -1,6 +1,15 @@
 package com.hjp.globaltester.control;
 
-public abstract class AbstractRemoteControlHandler implements RemoteControlHandler {
+/**
+ * This class is intended as the root of all remotely accessible control
+ * handlers. It provides sensible default behavior to be overridden if
+ * necessary.
+ * 
+ * @author mboonk
+ *
+ */
+public abstract class AbstractRemoteControlHandler implements
+		RemoteControlHandler {
 	@Override
 	public String getType() {
 		return this.getClass().getCanonicalName();
--- com.hjp.osgi.remotecontrol/src/com/hjp/globaltester/control/RemoteControlHandler.java
+++ com.hjp.osgi.remotecontrol/src/com/hjp/globaltester/control/RemoteControlHandler.java
@@ -1,11 +1,18 @@
 package com.hjp.globaltester.control;
 
+/**
+ * This is an interface for remotely accessible objects that can be used
+ * for external control.
+ * 
+ * @author mboonk
+ *
+ */
 public interface RemoteControlHandler {
 	/**
 	 * @return the unique identifier of this handler
 	 */
 	public String getIdentifier();
-	
+
 	/**
 	 * @return the type identifying the implemented interface
 	 */
